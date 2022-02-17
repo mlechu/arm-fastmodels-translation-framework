70938730dd6473a55b201d3f43274544d1629997
Reto Achermann <achreto@cs.ubc.ca>
2022-02-17 12:09:15 -0800

    state: adding a state representation
    This includes a notion of a generic abstract class for states,
    and state fields providing access to slices
    
    Signed-off-by: Reto Achermann <achreto@cs.ubc.ca>

---
+++ src/include/framework/state_field_base.hpp
@@ -0,0 +1,153 @@
+/**
+ * Represents the field of a translation unit state
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (C) 2022, Reto Achermann (The University of British Columbia)
+ */
+
+#ifndef _STATE_FIELD_BASE_H_
+#define _STATE_FIELD_BASE_H_ 1
+
+#include <map>
+#include <string>
+
+
+/**
+ * @brief Represents a state field
+ */
+class StateFieldBase {
+public:
+    /*
+     * -------------------------------------------------------------------------------------------
+     * Constructors
+     * -------------------------------------------------------------------------------------------
+     */
+
+
+    /**
+     * @brief constructs a default state field
+     */
+    StateFieldBase(std::string name, uint8_t nbits = 64, uint64_t reset_value = 0);
+
+
+    /**
+     * performs a rest on the field
+     */
+    void reset(void)
+    {
+        this->_value = this->_reset_value;
+    }
+
+    /*
+     * -------------------------------------------------------------------------------------------
+     * Debugging
+     * -------------------------------------------------------------------------------------------
+     */
+
+    /**
+     * @brief prints the field
+     */
+    void print_field(void);
+
+
+    /*
+     * -------------------------------------------------------------------------------------------
+     * Accessors
+     * -------------------------------------------------------------------------------------------
+     */
+
+
+    /**
+     * @brief returns the name of the field
+     *
+     * @returns a string representing the name of the field
+     */
+    const std::string &get_name(void) const
+    {
+        return this->_name;
+    }
+
+
+    /**
+     * @brief sets the value of the field
+     *
+     * @param[in] value  new value of the field
+     *
+     */
+    void set_value(uint64_t value)
+    {
+        this->_value = value & this->_mask;
+    }
+
+    /**
+     * @brief obtains the current value in the state field
+     *
+     * @return  the value of the field
+     */
+    uint64_t get_value(void)
+    {
+        return this->_value;
+    }
+
+
+    /*
+     * -------------------------------------------------------------------------------------------
+     * Slices
+     * -------------------------------------------------------------------------------------------
+     */
+
+
+    /**
+     * @brief obtains the current value in the state field
+     *
+     * @param[in] name   name of the field
+     * @param[in] value  pointer to where to store the field value
+     *
+     * @return true if the field value has been updated successfully
+     */
+    bool get_slice_value(const std::string &name, uint64_t *value);
+
+
+    /**
+     * @brief updates the value of the field slice
+     *
+     * @param[in] name   name of the field
+     * @param[] value    new value of the field
+     *
+     * @returns true if the field value has been updated successfully
+     */
+    bool set_slice_value(const std::string &name, uint64_t value);
+
+
+    /**
+     * @brief adds a new field slice
+     *
+     * @param[in] name   name of the slice
+     * @param[in] start  start bit of the slices (first bit = 0)
+     * @param[in] end    end bit of the slice (inclusive)
+     */
+    bool add_slice(const std::string &name, uint8_t start, uint8_t end);
+
+
+private:
+    ///< the name of the field
+    std::string _name;
+
+    ///< represents the current value of the field
+    uint64_t _value;
+
+    ///< represents the reset value of the field
+    uint64_t _reset_value;
+
+    ///< represents the width of the field in bits
+    uint8_t _bitwidth;
+
+    ///< the mask for this filed
+    uint64_t _mask;
+
+    ///< represents the slice map: slice name -> [start, end]
+    std::map<std::string, std::pair<uint8_t, uint8_t>> _slices;
+};
+
+#endif /* _STATE_FIELD_BASE_H_ */
---
+++ src/state_field_base.cpp
@@ -0,0 +1,133 @@
+/**
+ * The Register Base Class of the Translation Unit
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (C) 2022, Reto Achermann (The University of British Columbia)
+ */
+
+#include <assert.h>
+
+#include <framework/logging.hpp>
+#include <framework/types.hpp>
+#include <framework/state_field_base.hpp>
+
+
+StateFieldBase::StateFieldBase(std::string name, uint8_t nbits, uint64_t reset_value)
+{
+    if (nbits < 64) {
+        this->_bitwidth = nbits;
+        this->_mask     = (1ULL << nbits) - 1;
+    } else {
+        if (nbits > 64) {
+            Logging::warn("StateFieldBase::StateFieldBase: bitwidth too large. Setting to 64\n");
+        }
+        this->_bitwidth = 64;
+        this->_mask     = ~(0ULL);
+    }
+    this->_name        = name;
+    this->_reset_value = reset_value & this->_mask;
+    this->_value       = reset_value & this->_mask;
+
+    this->_slices = std::map<std::string, std::pair<uint8_t, uint8_t>>();
+}
+
+
+void StateFieldBase::print_field(void)
+{
+    Logging::info("% 16s     % 2u    0x%016lx    (0x%llx)", this->_name.c_str(), this->_bitwidth,
+                  this->_value, this->_reset_value);
+}
+
+bool StateFieldBase::add_slice(const std::string &name, uint8_t start, uint8_t end)
+{
+    if (this->_slices.contains(name)) {
+        Logging::error("StateFieldBase::add_slice: slice %s already exists\n", name);
+        return false;
+    }
+
+    if (start > end) {
+        Logging::error("StateFieldBase::add_slice: start > end\n");
+        return false;
+    }
+
+    // taking >= here, as the end bit is (N-1) for a bitwidth of N
+    if (end >= this->_bitwidth) {
+        Logging::error("StateFieldBase::add_slice: end > bitwidth\n");
+        return false;
+    }
+
+    // do overlap check
+    for (auto it = this->_slices.begin(); it != this->_slices.end(); it++) {
+        // case 1: end is smaller than the start bit, no overlap
+        if (it->second.first <= start && start <= it->second.second) {
+            Logging::error("StateFieldBase::add_slice: start overlaps with existing slice "
+                           "(start)\n");
+            return false;
+        }
+
+        if (it->second.first <= end && end <= it->second.second) {
+            Logging::error("StateFieldBase::add_slice: start overlaps with existing slice "
+                           "(end)\n");
+            return false;
+        }
+    }
+
+    // add it to the slices
+    this->_slices[name] = std::make_pair(start, end);
+
+    // all set!
+    return true;
+}
+
+
+bool StateFieldBase::get_slice_value(const std::string &name, uint64_t *value)
+{
+    if (!this->_slices.contains(name)) {
+        Logging::error("StateFieldBase::get_slice_value: slice %s does not exist\n", name);
+        return false;
+    }
+
+    // get the slice
+    auto slice = this->_slices[name];
+
+    // need to handle the 64-bit case (second = 63, first = 0)
+    uint8_t nbits = (slice.second - slice.first + 1);
+    if (nbits == this->_bitwidth) {
+        *value = this->_value & this->_mask;
+    } else {
+        assert(nbits < 64);
+        uint64_t slice_mask = (1ULL << nbits) - 1;
+
+        // extract the slice
+        *value = (this->_value >> slice.first) & (slice_mask);
+    }
+
+    return true;
+}
+
+bool StateFieldBase::set_slice_value(const std::string &name, uint64_t value)
+{
+    if (!this->_slices.contains(name)) {
+        Logging::error("StateFieldBase::set_slice_value: slice %s does not exist\n", name);
+        return false;
+    }
+
+    // get the slice
+    auto slice = this->_slices[name];
+
+    // need to handle the 64-bit case (second = 63, first = 0)
+    uint8_t nbits = (slice.second - slice.first + 1);
+    if (nbits == this->_bitwidth) {
+        this->_value = value & this->_mask;
+    } else {
+        assert(nbits < 64);
+        uint64_t slice_mask = (1ULL << nbits) - 1;
+
+        // update the slice value
+        this->_value = (this->_value & ~(slice_mask << slice.first))
+                       | ((value & slice_mask) << slice.first);
+    }
+
+    return true;
+}
